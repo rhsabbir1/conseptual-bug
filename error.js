9d95d6c5cbbae18c5761ec9e84ce32008cd12211
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-09 16:12:20 +0600

    tyoes of erroes

---
+++ error.js
@@ -0,0 +1,31 @@
+// sentex error 
+
+// const one = 1';
+// console.log(one)
+
+function abe() {
+    console.log('yes')
+}
+abe();
+
+// type error 
+const persone = {
+    name: 'Abul Hasan',
+}
+
+// console.log(persone.name()) TypeError
+
+// const input = 'Sabbir Rahman';
+// const input = 51; TypeError: input.toLocaleUpperCase is not a function
+// console.log(input.toLocaleUpperCase()) 
+
+
+// ReferenceError :
+const a = 45;
+// console.log(ab)
+
+function num (){
+    const one = 1;   
+}
+
+// console.log(one)
